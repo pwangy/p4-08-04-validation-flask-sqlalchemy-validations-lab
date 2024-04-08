5e5c00afba1cce23d7c75cca9f8736af5724b188
pwangy <blipsandclicks@gmail.com>
2024-04-07 19:37:36 -0700

    complete lab

--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+from multiprocessing import Value
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 db = SQLAlchemy()
@@ -11,10 +12,24 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    @validates('name')
+    def validate_name(self, _, name):
+        if not name:
+            raise ValueError('Name is required')
+        author = db.session.query(Author.id).filter_by(name = name).first()
+        if author is not None:
+            raise ValueError('Name must be unique')
+        return name
+
+    @validates('phone_number')
+    def validate_phone(self, _, phone_number):
+        if len(phone_number) != 10 or not phone_number.isdigit():
+            raise ValueError('Phone number must be 10 digits')
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
+
 
 class Post(db.Model):
     __tablename__ = 'posts'
@@ -27,8 +42,30 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    @validates('title')
+    def validate_title(self, _, title):
+        if not title:
+            raise ValueError('Title is required')
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(substring in title for substring in clickbait):
+            raise ValueError('Tite must include clickbait')
+        return title
 
+    @validates('content', 'summary')
+    def validate_length(self, _, string):
+        if(_ == 'content'):
+            if len(string) < 250:
+                raise ValueError('Post must be 250 or more characters long')
+        if (_ == 'summary'):
+            if len(string) > 250:
+                raise ValueError('Post summary must be 240 or more characters long')
+        return string
+
+    @validates('cateogry')
+    def validate_cateogry(self, _, category):
+        if category != 'Fiction' and category != 'Non-Fiction':
+            raise ValueError('Category must be either Fiction or Non-Fiction')
+        return category
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
